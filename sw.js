af1eb087fccec18a2f080c67cd12361dfaf2f78b
kwinproduction <kwinproduction@gmail.com>
2026-05-01 07:52:54 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const CACHE_NAME = 'kwin-ecard-v' + Date.now();
+// Version එක අතින් වෙනස් කරන්න (උදා: 1.1 සිට 1.2 ට)
+const CACHE_NAME = 'kwin-ecard-v1.1'; 
+
 const ASSETS = [
   './',
   './index.html',
@@ -6,12 +8,12 @@ const ASSETS = [
   'https://raw.githubusercontent.com/kwinproduction/K-Win-Ecard/main/logo.jpeg'
 ];
 
-// 1. Install Event - Assets Cache කිරීම
+// 1. Install Event
 self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('K-Win: Caching new assets...');
+      console.log('K-Win: Caching assets...');
       return cache.addAll(ASSETS);
     })
   );
@@ -22,7 +24,12 @@ self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('K-Win: Deleting old cache...', key);
+            return caches.delete(key);
+          }
+        })
       );
     })
   );
@@ -34,11 +41,16 @@ self.addEventListener('fetch', (e) => {
   e.respondWith(
     fetch(e.request)
       .then((res) => {
+        // අලුත් දත්ත ලැබුණොත් එය Cache එකට දමන්න
         const resClone = res.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, resClone));
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(e.request, resClone);
+        });
         return res;
       })
-      .catch(() => caches.match(e.request))
+      .catch(() => {
+        // Network නැතිනම් Cache එකෙන් ලබා දෙන්න
+        return caches.match(e.request);
+      })
   );
 });
-
