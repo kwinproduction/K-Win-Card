9402f893a631840c03a87e9421c5e25331f10b1e
kwinproduction <kwinproduction@gmail.com>
2026-05-05 22:14:52 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ඔබ දුන් code එක මෙතැනට ගැලපෙන ලෙස:
-const CACHE_NAME = 'kwin-ecard-v9' + Date.now();
+const CACHE_NAME = 'kwin-ecard-v10' + Date.now();
 const ASSETS = [
     './',
     './index.html',
